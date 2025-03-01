870a6a886f257b6665acad12fe09e15169c19efe
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-03-01 16:29:34 +0500

    feat: сделал страницу регистрации

--- src/pages/Register/RegisterPage.tsx
+++ src/pages/Register/RegisterPage.tsx
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useActionState } from 'react';
 
-const RegisterPage: React.FC = () => {
-  return <div>RegisterPage</div>;
+import { Input } from '@/app/components/ui/input';
+import { Button } from '@/app/components/ui/button';
+
+import { validateLogin } from './utils/validation';
+import { getErrorMessage } from './utils/getErrorMessage';
+import { FormFieldNames, type RegisterData } from './types';
+
+const initialState: RegisterData = {
+  email: '',
+  password: '',
+  errors: {},
 };
 
-export default RegisterPage;
+const formAction = async (_: RegisterData, formData: FormData) => {
+  const email = formData.get(FormFieldNames.EMAIL) as string;
+  const password = formData.get(FormFieldNames.PASSWORD) as string;
+
+  const validationResult = await validateLogin({ email, password });
+
+  if (validationResult.success) {
+    // @todo: здесь запрос на бэк и редирект на страницу входа
+
+    return { email, password };
+  } else {
+    const errorMessages: { [key: string]: string } = {};
+
+    validationResult.errors?.forEach(error => {
+      errorMessages[error.fieldName] = getErrorMessage(error.code);
+    });
+
+    return {
+      email,
+      password,
+      errors: errorMessages
+    };
+  }
+};
+
+const LoginPage: React.FC = () => {
+  const [state, action, pending] = useActionState<RegisterData, FormData>(formAction, initialState);
+
+  return (
+    <form action={action} className="border-[#D9D9D9] w-80 rounded-[2px] border p-6 shadow-md">
+      <label className="mb-1 flex flex-col gap-2">
+        <p className="text-base text-[#1E1E1E]">Email</p>
+        <Input
+          type="email"
+          placeholder="Email"
+          name={FormFieldNames.EMAIL}
+          className="w-full"
+          defaultValue={state.email}
+        />
+      </label>
+      {state.errors?.[FormFieldNames.EMAIL] && (
+        <p className="mb-3 text-sm text-red-500">{state.errors[FormFieldNames.EMAIL]}</p>
+      )}
+
+      <label className="mb-1 flex flex-col gap-2 mt-4">
+        <p className="text-base text-[#1E1E1E]">Password</p>
+        <Input
+          type="password"
+          placeholder="Password"
+          name={FormFieldNames.PASSWORD}
+          className="w-full"
+          defaultValue={state.password}
+        />
+      </label>
+      {state.errors?.[FormFieldNames.PASSWORD] && (
+        <p className="mb-3 text-sm text-red-500">{state.errors[FormFieldNames.PASSWORD]}</p>
+      )}
+
+      {state.errors?.general && (
+        <p className="mb-3 mt-2 text-sm text-red-500">{state.errors.general}</p>
+      )}
+
+      <Button
+        className="w-full mt-4"
+        type="submit"
+        disabled={pending}
+      >
+        Register
+      </Button>
+    </form>
+  );
+};
+
+export default LoginPage;
---
+++ src/pages/Register/types.ts
@@ -0,0 +1,12 @@
+export enum FormFieldNames {
+  EMAIL = 'email',
+  PASSWORD = 'password'
+}
+
+export interface RegisterData {
+  email: string;
+  password: string;
+  errors?: {
+    [key in FormFieldNames | 'general']?: string;
+  };
+}
