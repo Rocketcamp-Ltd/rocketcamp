1cfde705d9ee1552b8c0a2a7878b1ceaa0fa7f0f
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-03-01 16:01:58 +0500

    add: axios interceptors

--- src/app/api/client.ts
+++ src/app/api/client.ts
@@ -1,8 +1,35 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
 const client = axios.create({
   baseURL: 'http://localhost:3000/api/v1'
 });
+
+function requestSuccess(config: InternalAxiosRequestConfig): InternalAxiosRequestConfig {
+  const token = localStorage.getItem('Authorization');
+
+  if (token) {
+    config.headers.Authorization = token;
+  }
+
+  return config;
+}
+
+function requestError(error: AxiosError): Promise<never> {
+  return Promise.reject(error);
+}
+
+function responseSuccess<T>(response: AxiosResponse<T>): T {
+  return response.data;
+}
+
+function responseError(error: AxiosError): Promise<never> {
+  console.error('error >>>', error);
+  return Promise.reject(error);
+}
+
+client.interceptors.request.use(requestSuccess, requestError);
+client.interceptors.response.use(responseSuccess, responseError);
+
 
 export {
   client
