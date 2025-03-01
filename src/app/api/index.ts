c0e568a9b781bb89af4e89dfa83c44a81afde286
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-03-01 17:31:45 +0500

    add: модуль авторизации апи.

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,6 +1,8 @@
 import { client } from './client';
+import { authModule } from './modules/auth';
 import { courseModule } from './modules/course';
 
 export const api = {
   profile: courseModule(client),
+  auth: authModule(client),
 };
---
+++ src/app/api/modules/auth.ts
@@ -0,0 +1,25 @@
+import type { AxiosInstance } from 'axios';
+import type { User } from '@/types/user';
+
+interface LoginPayload {
+  email: string;
+  password: string;
+}
+
+export const authModule = (client: AxiosInstance) => ({
+  // @todo: return User & Tokens
+  login (payload: LoginPayload): Promise<User> {
+    return client.post('/auth/login', payload);
+  },
+
+  // @todo: any kaka
+  logout(): Promise<any> {
+    return client.post('/auth/logout');
+  },
+
+  // @todo: rename to register payload
+  // @todo: return User & Tokens
+  register(payload: LoginPayload): Promise<User> {
+    return client.post('/auth/register', payload);
+  }
+});
