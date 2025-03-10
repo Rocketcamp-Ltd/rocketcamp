f8aae8e27f3b16daa7f7a2616d162a703d067b76
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-03-10 15:23:35 +0500

    добил страничку конкретного курса

--- src/app/components/business/CourseCarousel.tsx
+++ src/app/components/business/CourseCarousel.tsx
@@ -5,7 +5,7 @@ import { CircleArrowRight } from 'lucide-react';
 import type { Course } from '@/types/courses';
 
 import { Carousel, CarouselPrevious, CarouselContent, CarouselItem, CarouselNext } from '../ui/carousel';
-import { Progress } from '../ui/progress';
+import { CourseCard } from './CourseCard';
 
 interface Props {
   title?: string;
@@ -27,24 +27,7 @@ export const CourseCarousel: React.FC<Props> = ({ title, seeAllLink, courses }) 
               className="mr-6 max-h-96 max-w-md rounded-xl border-1 border-[#D9D9D9] p-4 last:mr-0"
               key={course.id}
             >
-              <div>
-                <img
-                  src={course.cover}
-                  alt={course.title}
-                  className="object-cover"
-                />
-              </div>
-
-              <div className="mt-4 flex flex-col gap-2">
-                <p className="text-xl font-medium text-[#1E1E1E]">{course.title}</p>
-                <p className="text-base text-[#757575]">{course.subtitle}</p>
-              </div>
-
-              {Boolean(course.progress) && (
-                <div className="mt-4">
-                  <Progress value={course.progress} />
-                </div>
-              )}
+              <CourseCard course={course} />
             </CarouselItem>
           ))}
 
--- src/pages/CoursesByAlias/CoursesByAliasPage.tsx
+++ src/pages/CoursesByAlias/CoursesByAliasPage.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import type { Course } from '@/types/courses';
+import { CourseCard } from '@/app/components/business/CourseCard';
+import { coursesMock } from './mock';
 
 const CoursesByAliasPage: React.FC = () => {
-  return <div>CoursesByAliasPage</div>;
+  const [courses, setCourses] = useState<Course[]>(coursesMock);
+
+  return (
+    <div className="container mx-auto py-12">
+      <div className="grid grid-cols-3 gap-y-3 gap-x-5">
+        {courses.map(item => {
+          return (
+            <div key={item.id} className='max-h-96 rounded-xl border-1 border-[#D9D9D9] p-4'>
+              <CourseCard course={item} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default CoursesByAliasPage;
