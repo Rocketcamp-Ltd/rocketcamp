02e080a513a9b8e1494df4e57420caa323b77e42
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2025-03-10 15:25:37 +0500

    feat: добавил заголовок к курсу

--- src/pages/CoursesByAlias/CoursesByAliasPage.tsx
+++ src/pages/CoursesByAlias/CoursesByAliasPage.tsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
-import type { Course } from '@/types/courses';
+import type { Course, CourseCategory } from '@/types/courses';
 import { CourseCard } from '@/app/components/business/CourseCard';
 import { coursesMock } from './mock';
 
 const CoursesByAliasPage: React.FC = () => {
   const [courses, setCourses] = useState<Course[]>(coursesMock);
+  const [category, setCategory] = useState<CourseCategory>({
+    id: 1,
+    name: 'Name',
+    alias: 'title1',
+  });
 
   return (
     <div className="container mx-auto py-12">
-      <div className="grid grid-cols-3 gap-y-3 gap-x-5">
+      <h1 className="mb-4 text-3xl font-medium text-black">{category.name}</h1>
+
+      <div className="grid grid-cols-3 gap-x-5 gap-y-3">
         {courses.map(item => {
           return (
-            <div key={item.id} className='max-h-96 rounded-xl border-1 border-[#D9D9D9] p-4'>
+            <div
+              key={item.id}
+              className="max-h-96 rounded-xl border-1 border-[#D9D9D9] p-4"
+            >
               <CourseCard course={item} />
             </div>
           );
